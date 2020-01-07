e42c699aefd1cfb538197a00632e304c8bbb95b4
wagner <wagnerricardonet@gmail.com>
2020-01-06 22:18:08 -0300

    initial commit

---
+++ src/test/java/com/wagnerww/apitraining/ApiTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wagnerww.apitraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
