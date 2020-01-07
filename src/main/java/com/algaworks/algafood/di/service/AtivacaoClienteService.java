fbcecdd64eb530c87d63846eb8fdfe0079154421
wagnerww <wagnerricardonet@gmail.com>
2020-01-07 13:25:49 -0300

    verificando ambiguidade de classes

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.di.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -8,18 +11,16 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
+	@Qualifier("email")
+	@Autowired
 	private Notificador notificador;
-	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
+		
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
+	
 	
 }
