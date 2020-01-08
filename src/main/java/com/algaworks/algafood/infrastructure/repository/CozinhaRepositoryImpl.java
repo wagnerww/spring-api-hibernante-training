5096f206437e21ca786d13069a1039fc51a7c026
wagnerww <wagnerricardonet@gmail.com>
2020-01-08 13:23:36 -0300

    infrastructure ddd

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+	@Override
+	public List<Cozinha> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cozinha buscar(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cozinha salvar(Cozinha cozinha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(Cozinha cozinha) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
