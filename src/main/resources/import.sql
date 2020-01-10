0d8d2839461504008ed24878e2aba96e6440ef57
wagnerww <wagnerricardonet@gmail.com>
2020-01-10 14:05:48 -0300

    restaurants crud

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandeza');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome) values ('Gulz');
+insert into restaurante (nome) values ('Tio Arno');
